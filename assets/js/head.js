f9df85694623f404f1365580a7338b7f69b8f096
Jim Forsyth <jim@avakore.com>
2021-09-10 16:14:59 +0100

    Added SaaS Mantra favicon for debugging.

---
+++ assets/js/head.js
@@ -0,0 +1,4 @@
+<!-- start @ favicon -->
+<link rel="icon" href="https://saasmantra.com/images/favicon.ico" />
+<script>document.querySelector("link[rel*='icon']").href = "https://saasmantra.com/images/favicon.ico";</script>
+<!-- end @ favicon -->
